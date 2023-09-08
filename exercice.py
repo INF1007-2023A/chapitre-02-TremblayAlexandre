4bf15e8c62a34071e9a7afb291a88598cf496e7f
Wertubald <101939619+Wertubald@users.noreply.github.com>
2023-09-08 09:39:24 -0400

    Réponse ex Chp 2

--- exercice.py
+++ exercice.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
+    
+    for i in range(len(mot)):
+        mot=mot[:i]+ chr(ord(mot[i])-32) + mot[i+1:]
     return mot
 
 
@@ -19,3 +22,5 @@ if __name__ == '__main__':
         mots[i] = majuscule(mots[i])
 
     print(mots)
+    
+
